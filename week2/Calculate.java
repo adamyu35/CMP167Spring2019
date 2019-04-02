bcaa1249893c13590b740f5c8269d9b7f397df62
adamyu35 <47425924+adamyu35@users.noreply.github.com>
2019-04-02 12:22:47 -0400

    Add files via upload

---
+++ week2/Calculate.java
@@ -0,0 +1,19 @@
+/*
+ * Author: Adam 
+ * Created: 4/2/19
+ * Description: This class has calculate methods
+ */
+public class Calculate {
+
+
+	public static int multiply(int a , int b) {
+		return a*b;
+	}
+	public static double multiply(double a , double b) {
+		return a*b;
+}
+	public static double getRectangleArea(double width, double length) {
+		return width*length;
+	}
+}
+		
